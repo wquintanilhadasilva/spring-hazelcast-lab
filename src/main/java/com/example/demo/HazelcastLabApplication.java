61deebd550d0159c4eb2d51366690102b1abb5a4
Wedson Quintanilha da Silva <wedson.silva@basis.com.br>
2021-02-12 18:36:03 -0300

    add configuration on startup

--- src/main/java/com/example/demo/HazelcastLabApplication.java
+++ src/main/java/com/example/demo/HazelcastLabApplication.java
@@ -2,9 +2,11 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableConfigurationProperties({ })
 @EnableCaching
 public class HazelcastLabApplication {
 
